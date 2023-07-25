675af7d98c2c87c8b3c79e8ae937d23431b28280
John Kimani <jwangach@gmail.com>
2023-07-25 12:57:37 +0300

    static front

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const App = () => {
+(() => {
+    let date = new Date();
+    let currentYear = date.getFullYear();
+    return currentYear;
+  })();
+  
   return (
     <>
       <header>
@@ -22,11 +28,11 @@ const App = () => {
       </header>
       <main className='mt-12 translate-y-7'>
         <section className='organizer grid gap-4'>
-          <div className='savings border-double border-2 border-slate-600 p-5'>
-            1
+          <div data-modal-target="savings-modal" data-modal-toggle="savings-modal" className='savings border-double border-2 border-slate-600 p-5'>
+            <h3 className='text-center font-bold uppercase underline'>Savings</h3>
           </div>
-          <div className='investments border-double border-2 border-slate-600 p-5'>
-            2
+          <div data-modal-target="invest-modal" data-modal-toggle="invest-modal" className='investments border-double border-2 border-slate-600 p-5'>
+            <h3 className='text-center font-bold uppercase underline'>Investments</h3>
           </div>
           <div className='actions border-double border-2 border-slate-600 p-5'>
             <div className='bg-slate-700 p-4 text-white text-center rounded m-2'>
@@ -42,14 +48,17 @@ const App = () => {
             <span>Earnings</span>
             </div>
           </div>
-          <div className='expense border-double border-2 border-slate-600 p-5'>
-            4
+          <div data-modal-target="expenses-modal" data-modal-toggle="expenses-modal" className='expense border-double border-2 border-slate-600 p-5'>
+            <h3 className='text-center font-bold uppercase underline'>Expenses</h3>
           </div>
-          <div className='earning border-double border-2 border-slate-600 p-5'>
-            5
+          <div data-modal-target="earnings-modal" data-modal-toggle="earnings-modal" className='earning border-double border-2 border-slate-600 p-5'>
+            <h3 className='text-center font-bold uppercase underline'>Earnings</h3>
           </div>
         </section>
       </main>
+      <footer className='bg-slate-800 border border-slate-800 p-6 fixed bottom-0 w-full'>
+        <p className='text-white font-bold text-center uppercase'>Budgetry 2023</p>
+      </footer>
     </>
   )
 }
